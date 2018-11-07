4e5e1c4edd443c5fd5a3cf81fe0d21a0cd5df49e
Jon Reinhold <jon.reinhold15@gmail.com>
2018-11-06 22:56:21 -0600

    Update HelloClient.py

--- expanded-hello-world/HelloClient.py
+++ expanded-hello-world/HelloClient.py
@@ -1,4 +1,16 @@
 import socket
+import Crypto
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+
+BLOCK_SIZE = 32
+
+def encrypto(message):
+        raw = AES.new(b'thetestkey123456', AES.MODE_CBC, b'thetestIV1234567')
+        encrypted_msg = raw.encrypt(pad(message, BLOCK_SIZE))
+
+        return encrypted_msg
+
 
 # hard code private IP of linux box
 ip = '192.168.1.191'
@@ -11,6 +23,10 @@ s.connect((ip, port))
 message = input("Enter path to source file(s): ")
 stream_file = open(message, "rb")
 stream = stream_file.read(1024)
+
+print(stream)
+
+stream = encrypto(stream)
 
 print(stream)
 
